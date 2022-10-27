01951f5ac501e0f55271521f99a8ac9c45de0f86
marcoaufiero <marcoaufiero96@gmail.com>
2022-10-27 16:12:00 +0200

    Second exercise : js user choice function added

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -4,3 +4,25 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+let inputChoice = prompt("Inserisci 'p' se vuoi scergliere pari o 'd' se vuoi scegliere dispari");
+let even;
+let odd;
+
+if(inputChoice == 'p'){
+    inputChoice = even;
+    console.log('Hai scelto pari')
+}else if(inputChoice == 'd'){
+    inputChoice = odd;
+    console.log('Hai scelto dispari')
+}else{
+    console.log('Scelta non valida')
+}
+
+let userNumber = parseInt(prompt('Insrisci un numero da 1 a 5'));
+console.log(`Il tuo numero è : ${userNumber}`);
+
+function cpuNumber(min,max){
+
+    
+}
